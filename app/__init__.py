1c33d29240ffb12b27a962f7bf3f5d5749bc0134
Marcus Pereira <marcus@negros.dev>
2022-06-11 11:16:57 -0300

    Instalando dynaconf

--- app/__init__.py
+++ app/__init__.py
@@ -1,17 +1,9 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
-from app import routes
-
-db = SQLAlchemy()
+from dynaconf import FlaskDynaconf
 
 def create_app():
   app = Flask(__name__)
-  app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'
-  app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-  db.init_app(app)
-  Migrate(app, db)
-  routes.init_app(app)
+  FlaskDynaconf(app, extensions_list=True)
 
   from app.models import Channel
 
--- app/models.py
+++ app/models.py
@@ -1,4 +1,4 @@
-from app import db
+from app.extensions import db
 
 class Channel(db.Model):
     __tablename__ = "channels"
