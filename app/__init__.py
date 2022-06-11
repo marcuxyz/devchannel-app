444d13cfdde6533a4593316e786987179e03b8b6
Marcus Pereira <marcus@negros.dev>
2022-06-11 11:07:53 -0300

    Criando modelo Channel

--- app/__init__.py
+++ app/__init__.py
@@ -3,13 +3,16 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from app import routes
 
+db = SQLAlchemy()
 
 def create_app():
   app = Flask(__name__)
   app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'
   app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-  db = SQLAlchemy(app)
+  db.init_app(app)
   Migrate(app, db)
   routes.init_app(app)
+
+  from app.models import Channel
 
   return app
---
+++ app/models.py
@@ -0,0 +1,15 @@
+from app import db
+
+class Channel(db.Model):
+    __tablename__ = "channels"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False, unique=True)
+    url = db.Column(db.String, nullable=False, unique=True)
+    description = db.Column(db.Text)
+    country = db.Column(db.String(5), nullable=False)
+    video = db.Column(db.String)
+    image = db.Column(db.LargeBinary)
+
+    def __repr__(self) -> str:
+        return self.name
