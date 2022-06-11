48c849ea0c1aef25fd89c3525d33ed313368f27c
Marcus Pereira <marcus@negros.dev>
2022-06-11 10:56:48 -0300

    Configurando banco de dados

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,15 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from app import routes
+
 
 def create_app():
   app = Flask(__name__)
+  app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'
+  app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+  db = SQLAlchemy(app)
+  Migrate(app, db)
   routes.init_app(app)
 
   return app
