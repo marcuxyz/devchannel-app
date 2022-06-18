a093660d3e1b9dc7ce934f4654ffd66c282d2d69
Marcus Pereira <marcus@negros.dev>
2022-06-18 10:37:57 -0300

    Configurando conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,4 +9,11 @@ def client():
     app.testing = True
     app_context = app.test_request_context()
     app_context.push()
-    return app.test_client()
+
+    with app.test_client() as client:
+        db.create_all()
+        
+        yield client
+
+        db.session.remove()
+        db.drop_all()
